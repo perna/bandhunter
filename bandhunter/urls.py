0d2b4d0901bc39966338eeb1a9f2c8213a7f8693
Fernando Giovanini <fernando@giovanini.com.br>
2017-01-20 18:11:53 -0200

    Add media directory to urls.py

--- bandhunter/urls.py
+++ bandhunter/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, static
 from django.contrib import admin
 from customaccount.views import SignupView
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,4 +24,4 @@ urlpatterns = [
     url(r"^account/signup/$", SignupView.as_view(), name="account_signup"),
     url(r"^account/", include("account.urls")),
     url(r"^dashboard/", include('dashboard.urls')),
-]
+] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
